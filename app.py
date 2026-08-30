fda2effa3ea6d5c49a5f90a9e499bd56a26afa14
2312552-png <2312552@dlu.edu.vn>
2026-08-30 16:54:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from datetime import date, timedelta
 import streamlit as st
 
 st.set_page_config(
-    page_title="Đăng ký vay vốn mua xe",    
+    page_title="Đăng ký vay vốn mua xe",
     page_icon="🚗",
     layout="centered"
 )
+
 st.image("logo.jpg", width=150)
+
 st.title("🚗 ĐƠN ĐĂNG KÝ VAY VỐN MUA XE")
 st.write("Vui lòng điền đầy đủ thông tin đề nghị vay vốn dưới đây.")
 
