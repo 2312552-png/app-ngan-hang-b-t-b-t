0be1e4549425a2cf639916202b42d213b9130c32
2312552-png <2312552@dlu.edu.vn>
2026-08-30 17:10:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ st.set_page_config(
 )
 # CHÈN VÀO ĐÂY (Ngay trên tiêu đề):
 st.image("logo.jpg", width=150)
+
 st.title("🚗 ĐƠN ĐĂNG KÝ VAY VỐN MUA XE")
 st.write("Vui lòng điền đầy đủ thông tin đề nghị vay vốn dưới đây.")
 
